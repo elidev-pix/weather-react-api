64dcaec1ef30ff189ab2348f68c3b6d547e9e650
Eliel Sawadogo <elielrsawadogo@gmail.com>
2026-08-16 16:26:12 +0000

    First design (basics)

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+
+const api = {
+    key : "cecf38c2f1ddeda0f273cb4a378bd449",
+    base : "https://api.openweathermap.org/data/2.5/"
+  }
+
+function App() {
+
+  const [search, setSearch] = useState("Ouagadougou");
+  const [weather, setWeather] = useState({})
+
+  const searchPressed = (city = "Ouagadougou") => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}&lang=fr`)
+    .then( response => response.json())
+    .then(result => {
+      setWeather(result);
+      console.log(result)
+    });
+  }
+
+   useEffect(() => {
+    searchPressed()
+  }, [])
+
+  return (
+    <div className='min-h-screen justify-center items-center '>
+      <div className="flex flex-col px-2 py-3 rounded-md justify-center items-center">
+        <div className="flex flex-col">
+          <div className="flex gap-2">
+            <a href='https://github.com/elidev-pix'>elidev-pix</a>
+            <span>Weather App</span>
+          </div>
+          <span className="italic">A React-powered weather application built with real-time weather APIs</span>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex">
+            <input 
+              type="text" 
+              placeholder="Rechercher"
+              onChange={(e)=>setSearch(e.target.value)}
+            />
+            <button 
+              className=""
+              onClick={searchPressed}
+            >
+              Rechercher...
+            </button>
+          </div>
+          
+
+          <p>Ville : {weather.name ?? ""}</p>
+          <p>Pays : {weather.sys?.country ?? ""}</p>
+
+          <span>Latitude : {weather.coord?.lat ?? ""}</span>
+          <span>Longitude : {weather.coord?.lon ?? ""}</span>
+
+          <p>Température : {weather.main?.temp ?? ""}</p>
+
+          <p>{weather.weather?.[0]?.main ?? ""}</p>
+          <p>{weather.weather?.[0]?.description ?? ""}</p>
+
+        </div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
